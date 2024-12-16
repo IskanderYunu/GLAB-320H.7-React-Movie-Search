806f3eba7b8ce30534d17c839e907db16b70b87b
iskanderyunu <izzyyunu@gmail.com>
2024-12-16 14:14:48 -0500

    updated correct format

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,44 +1,38 @@
 import { useState, useEffect } from "react";
-import logo from "./logo.svg";
+import Form from "./components/Form";
+import MovieDisplay from "./components/MovieDisplay";
 import "./App.css";
 
-import MovieDisplay from "./components/MovieDisplay";
-import Form from "./components/Form";
-
-export default function App() {
+function App() {
+  // Constant with your API Key
   const apiKey = "3ea8220e";
 
+  // State to hold movie data
   const [movie, setMovie] = useState(null);
 
+  // Function to get movies
   const getMovie = async (searchTerm) => {
-    const response = await fetch(
-      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
-    );
-    const data = await response.json();
-    setMovie(data);
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch (e) {
+      console.error(e);
+    }
   };
-
-  // This will run on the first render but not on subsquent renders
   useEffect(() => {
     getMovie("Clueless");
   }, []);
-
   return (
-    <div className="App">
-      <Form moviesearch={getMovie} />
-      <MovieDisplay movie={movie} />
-    </div>
+    <>
+      <div className="App">
+        <Form moviesearch={getMovie} />
+        <MovieDisplay movie={movie} />
+      </div>
+    </>
   );
 }
 
-const getMovie = async (searchTerm) => {
-  try {
-    const response = await fetch(
-      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
-    );
-    const data = await response.json();
-    setMovie(data);
-  } catch (e) {
-    console.error(e);
-  }
-};
+export default App;
